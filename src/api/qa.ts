85e0b0c91d801aee11678b9b9588e029fa744b4e
FICN <fnicen@qq.com>
2024-05-03 13:50:58 +0800

    feat: 完成数据集管理（增删改）

--- src/api/qa.ts
+++ src/api/qa.ts
@@ -10,10 +10,22 @@ export type qaRes = {
     }
   ];
 };
+export type qaChangeRes = {
+  success: boolean;
+};
 
 export const getQaLength = () => {
   return http.request<number>("get", baseUrlApi("getQaLength"));
 };
 export const getQa = (data?: number) => {
   return http.request<qaRes>("post", baseUrlApi("getQa"), { data });
+};
+export const addQa = (data?: object) => {
+  return http.request<qaChangeRes>("post", baseUrlApi("addQa"), { data });
+};
+export const delQa = (data?: object) => {
+  return http.request<qaChangeRes>("post", baseUrlApi("delQa"), { data });
+};
+export const updateQa = (data?: object) => {
+  return http.request<qaChangeRes>("post", baseUrlApi("updateQa"), { data });
 };
--- src/views/dataset/index.vue
+++ src/views/dataset/index.vue
@@ -1,14 +1,28 @@
 <script setup lang="ts">
 import { ref, reactive, onMounted } from "vue";
 import { delay } from "@pureadmin/utils";
-import { getQa, getQaLength } from "@/api/qa";
+import { message } from "@/utils/message";
+import { getQa, getQaLength, addQa, delQa, updateQa } from "@/api/qa";
 defineOptions({
   name: "Dataset"
 });
 
 const loading = ref(false);
 const dataList = ref([]);
+const isEdit = ref(false);
+const editWindow = ref(false);
+const delWindow = ref(false);
+const selectedItem = ref(null);
+const formItem = ref({
+  q: "",
+  a: ""
+});
 const columns: TableColumnList = [
+  {
+    label: "ID",
+    prop: "id",
+    hide: true
+  },
   {
     label: "问题",
     prop: "q",
@@ -18,6 +32,12 @@ const columns: TableColumnList = [
     label: "答案",
     prop: "a",
     minWidth: 200
+  },
+  {
+    headerSlot: "operateHeader",
+    label: "操作",
+    fixed: "right",
+    slot: "operation"
   }
 ];
 /** 加载动画配置 */
@@ -68,6 +88,71 @@ onMounted(async () => {
   dataList.value = qas.data;
   loading.value = false;
 });
+
+const editClick = row => {
+  if (!isEdit.value) {
+    formItem.value.q = "";
+    formItem.value.a = "";
+    message("请填写问题和答案", { type: "warning" });
+    editWindow.value = true;
+    return;
+  }
+  selectedItem.value = row.row;
+  formItem.value.q = row.row.q;
+  formItem.value.a = row.row.a;
+  editWindow.value = true;
+  console.log("edit", selectedItem.value);
+};
+
+const del = row => {
+  selectedItem.value = row.row;
+  delWindow.value = true;
+  console.log("del", row);
+};
+
+const editConfirm = async () => {
+  console.log("editConfirm", selectedItem.value);
+  let res = await updateQa({
+    id: selectedItem.value.id,
+    q: formItem.value.q,
+    a: formItem.value.a
+  });
+  if (res.success) {
+    message("修改成功", { type: "success" });
+    editWindow.value = false;
+    onCurrentChange(pagination.value.currentPage);
+    return;
+  }
+  message("修改失败", { type: "error" });
+};
+
+const delConfirm = async () => {
+  console.log("delConfirm", selectedItem.value);
+  let res = await delQa({ id: selectedItem.value.id });
+  if (res.success) {
+    message("删除成功", { type: "success" });
+    delWindow.value = false;
+    onCurrentChange(pagination.value.currentPage);
+
+    return;
+  }
+  message("删除失败", { type: "error" });
+};
+
+const addConfirm = async () => {
+  console.log("addConfirm", formItem.value);
+  let res = await addQa({
+    q: formItem.value.q,
+    a: formItem.value.a
+  });
+  if (res.success) {
+    message("添加成功", { type: "success" });
+    editWindow.value = false;
+    onCurrentChange(pagination.value.currentPage);
+    return;
+  }
+  message("添加失败", { type: "error" });
+};
 </script>
 
 <template>
@@ -81,13 +166,79 @@ onMounted(async () => {
       alignWhole="center"
       showOverflowTooltip
       size="large"
-      :height="800"
+      :height="600"
       :loading="loading"
       :loading-config="loadingConfig"
       :data="dataList"
       :columns="columns"
       :pagination="pagination"
       @current-change="onCurrentChange"
-    />
+    >
+      <template #operation="row">
+        <el-button
+          type="primary"
+          @click="
+            isEdit = true;
+            editClick(row);
+          "
+          >编辑</el-button
+        >
+        <el-button type="danger" @click="del(row)">删除</el-button>
+      </template>
+    </pure-table>
+    <div class="w-3/5 flex">
+      <el-button
+        type="primary"
+        size="large"
+        style=" flex-basis: 100%;font-size: 20px"
+        @click="
+          isEdit = false;
+          editClick({ row: {} });
+        "
+        >新增问答数据</el-button
+      >
+    </div>
   </div>
+  <el-dialog
+    v-model="editWindow"
+    :title="isEdit ? '编辑问答对' : '添加问答对'"
+    width="500"
+  >
+    <el-form :model="formItem">
+      <el-form-item label="问题">
+        <el-input
+          v-model="formItem.q"
+          type="textarea"
+          autocomplete="off"
+          autosize
+        />
+      </el-form-item>
+      <el-form-item label="回答">
+        <el-input
+          v-model="formItem.a"
+          type="textarea"
+          autocomplete="off"
+          autosize
+        />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="editWindow = false">取消</el-button>
+        <el-button
+          type="primary"
+          @click="isEdit ? editConfirm() : addConfirm()"
+        >
+          确定
+        </el-button>
+      </div>
+    </template>
+  </el-dialog>
+  <el-dialog v-model="delWindow" title="删除问答对" width="300">
+    <span>确认删除该问答对吗？</span>
+    <template #footer>
+      <el-button @click="delWindow = false">取 消</el-button>
+      <el-button type="danger" @click="delConfirm"> 确 定 </el-button>
+    </template>
+  </el-dialog>
 </template>
